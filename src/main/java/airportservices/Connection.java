638713aded6d26337bd95a46af2801a22a343659
Arvind Kumar <105741251+arparvind101@users.noreply.github.com>
2022-08-02 11:46:16 +0530

    AIrTicketReservationSystem

---
+++ src/main/java/airportservices/Connection.java
@@ -0,0 +1,22 @@
+package airportservices;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;  
+
+public class connection{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c = DriverManager.getConnection("jdbc:mysql:///project4","root","");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
---
+++ src/main/java/com/example/demo/ArvindAirTicketReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArvindAirTicketReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArvindAirTicketReservationSystemApplication.class, args);
+	}
+
+}
